5f511dbedb7704b96ef98726a57d8746b44cd14c
XCwosjw <53867435+XCwosjw@users.noreply.github.com>
2020-03-05 17:02:46 +0800

    Update FunnyTitle.js

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,12 +2,10 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/funny.ico");
         document.title = '°(°ˊДˋ°) °你怎么走啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = '(ฅ>ω<*ฅ) 噫你又回来啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
